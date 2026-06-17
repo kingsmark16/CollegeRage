89200d83ba5cee6fa02facc05734dce0c8b587c1
KingsMark <kingsmark16@users.noreply.github.com>
2026-06-17 21:28:22 +0800

    configure docker build

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,5 +4,5 @@ import { createServer } from "./server";
 const server = createServer();
 
 server.listen(env.PORT, '0.0.0.0',() => {
-    console.log(`Servr is running on port ${env.PORT}`);
+    console.log(`Server is running on port ${env.PORT}`);
 })
