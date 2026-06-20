b7135fa18d1e9c72fc4bb9889e405711c3bade4e
KingsMark <kingsmark16@users.noreply.github.com>
2026-06-20 13:22:48 +0800

    reset

--- server/src/index.ts
+++
@@ -1,8 +0,0 @@
-import env from "./config/env";
-import { createServer } from "./server";
-
-const server = createServer();
-
-server.listen(env.PORT, '0.0.0.0',() => {
-    console.log(`Server is running on port ${env.PORT}`);
-})
